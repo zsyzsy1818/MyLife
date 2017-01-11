9172fb3097f97f31fb7559ea48928c0ed698a6b5
Yu <zsyzsy1818@hotmail.com>
2017-01-11 15:59:11 +0800

    Initial commit

---
+++ app/src/main/java/com/yu/mylife/CustomApplication.java
@@ -0,0 +1,17 @@
+package com.yu.mylife;
+
+import android.app.Application;
+
+/**
+ * @author :Yu
+ * @version :
+ * @Date : 2017/1/11
+ * @Description :
+ */
+public class CustomApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
